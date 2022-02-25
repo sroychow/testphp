061f8b98fcb1d194d666fd27b5f7f83ab2e0cbdd
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2022-02-25 14:27:01 +0100

    update startup script for cvmfs

---
+++ openshift-start-up-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+which python
+
+curl https://ecsft.cern.ch/dist/cvmfs/cvmfs-2.9.0/cvmfs-service-2.9.0-1.x86_64.docker.tar.gz | docker load
+docker run -d --rm \
+  -e CVMFS_CLIENT_PROFILE=single \
+  -e CVMFS_REPOSITORIES=sft.cern.ch,cms.cern.ch \
+  --cap-add SYS_ADMIN \
+  --device /dev/fuse \
+  --volume /cvmfs:/cvmfs:shared \
+  cvmfs/service:2.8.0-1
