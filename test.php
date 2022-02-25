38e036ee1cb6cfbdae4208c3521b3c4c2bdd1b94
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2022-02-25 15:24:20 +0100

    playing1

--- test.php
+++ test.php
@@ -4,4 +4,11 @@ echo "<pre>$out</pre>";
 
 $outo = exec('ls -l');
 echo "<pre>$outo</pre>";
+
+$outo = exec('ls /home');
+echo "<pre>$outo</pre>";
+
+$outo = exec('ls /tmp');
+echo "<pre>$outo</pre>";
+
 ?>
