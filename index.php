c8c766a3e8e3ba27a39e3cb7516efc346d02504a
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2022-02-28 16:16:21 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$out = exec('pwd');
+echo "<pre>$out</pre>";
+
+$outo = exec('ls /eos/cms/store/group/tracker-cctrack/www/CondDBMonitoringWebInterface/CondDBMonitoring/pro/CMS_CONDITIONS/DBTagCollection/SiStripFedCabling/SiStripFedCabling_GR10_v1_hlt/CablingLog/');
+echo "<pre>$outo</pre>";
+
+?>
